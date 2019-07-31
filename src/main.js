2e2a25a48b0320a81f9b62746049a8d00bdf68be
soxiuzi <870858967@qq.com>
2019-07-31 17:28:08 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -6,8 +6,22 @@ import AlloyFinger from 'alloyfinger/alloy_finger' // 引入手势库
 import AlloyFingerPlugin from 'alloyfinger/vue/alloy_finger_vue' // 引入手势库
 import './utils/validator' // 引入表单验证规则
 import './utils/customValidator'
-import VueI18n from 'vue-i18n'
 import 'normalize.css'
+import fastclick from 'fastclick' // 快速点击，取消移动端300ms延迟
+import VConsole from 'vconsole' // 控制台
+import VueLazyload from 'vue-lazyload'
+
+// Vue.use(VueLazyload, {
+//   preload: 1.3, // 预压高度比例
+//   error: 'xxx.png', // 显示错误的图标
+//   loading: 'xxx.png', // 图片正在加载时显示的图片
+//   attempt: 1 // 图像尝试加载次数
+// })
+
+
+const vConsole = new VConsole()
+
+fastclick.attach(document.body)
 
 Vue.config.productionTip = false
 
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -2,19 +2,32 @@
   <div class="login">
     <input v-validate="'required|phone'" name="phone">
     <span v-show="errors.has('phone')">{{ errors.first('phone') }}</span>
+    <div class="wrapper" ref="wrapper">
+      <ul class="content">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+      </ul>
+    </div>
+    <h1>{{ testValue }}</h1>
   </div>
 </template>
 
 <script>
 //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 //例如：import 《组件名称》 from '《组件路径》';
+import BScroll from 'better-scroll'
 
 export default {
   //import引入的组件需要注入到对象中才能使用
   components: {},
   data() {
     //这里存放数据
-    return {};
+    return {
+      testValue: '变化前'
+    };
   },
   //监听属性 类似于data概念
   computed: {},
@@ -36,10 +49,17 @@ export default {
   },
   //生命周期 - 创建完成（可以访问当前this实例）
   created() {
+    setTimeout(function() {
+      this.testValue = '变化后'
+    }, 2000);
     console.log("登录页");
   },
   //生命周期 - 挂载完成（可以访问DOM元素）
-  mounted() {},
+  mounted() {
+    this.$nextTick(() => {
+      this.scroll = new BScroll(this.$refs.wrapper, {})
+    })
+  },
   beforeCreate() {}, //生命周期 - 创建之前
   beforeMount() {}, //生命周期 - 挂载之前
   beforeUpdate() {}, //生命周期 - 更新之前
